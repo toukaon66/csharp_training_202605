d6ed2fc45422e68f31d9a85323cf6c2911f97305
toukaon66 <tou.kaon.howsplan@gmail.com>
2026-05-21 10:50:17 +0900

    テスト

--- src/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace WebApp_Sample.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
